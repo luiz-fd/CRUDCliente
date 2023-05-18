91341e481b6a42203ea04c2217daaebb397e5b78
Luiz F Dias <luizfd@gmail.com>
2023-05-18 20:38:16 -0300

    First commit

---
+++ src/test/java/com/luizfd/CRUDCliente/CrudClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizfd.CRUDCliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
